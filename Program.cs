ad9a29e6e53b7e788acb5cbe2b938eac3b58b5d3
Lollypop-stack <kucovalexandr0@gmail.com>
2026-05-13 19:30:14 +0200

    Цикл foreach, создание функций, работа с файлами (создание,чтение, запись)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic; //для коллекций типо массивов и списков
+using System.IO; //для работы с файлами (создание, запись, закрытие и считывание)
 namespace project
 {
     class Pervui
@@ -91,7 +92,7 @@ namespace project
                 int shirina = Convert.ToInt32(Console.ReadLine());
                 if (shirina == 1488)
                 {
-                    Console.WriteLine("OH NO, NIGGA KITLER -_-");
+                    Console.WriteLine("OH NO, NIce LER -_-");
                     break;
                 }
                 else
@@ -226,6 +227,146 @@ namespace project
                 }
                 Console.Write("\n");
             }*/
+
+            /* Цикл foreach для работы с массивами
+            short[,] nums =
+            {
+                {12,2,1,12,2},
+                {6,2,4,2,123},
+                {1,2,16,15,2},
+                {11,2,13,34,122}
+            };
+            foreach (short el in nums)
+                Console.Write(el); 
+            List<int> nazvanie = [3, 5, 6];
+            List<string> texe = ["nina", "nana", "nunu"];
+            List<int> asd = new List<int>() { 1, 1, 1, 1, 1, 1 };
+            // foreach (int el in asd)
+            // {
+            //     Console.WriteLine(el);
+            // }
+            texe.Add(Convert.ToString(44));
+            nazvanie.Add(23);
+            texe.Add(Convert.ToString(3));
+            //    Методы для списков: 
+            nazvanie.Add(15);
+            asd.Remove(1);
+            texe.Sort();
+            texe.Reverse();
+            // texe.Find("nuna");
+            string? e = texe.Find(e => e.StartsWith("n"));
+            Console.WriteLine(e);
+            // foreach (string el in texe)
+            // {
+            //     Console.WriteLine(el);
+            // } */
+
+
+            /* Пробный вызов функции из мейна
+            Console.WriteLine("Введите первое число: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваш результат после сложения: ");
+            calc(first, second); */
+
+            /* Запись результата ретёрна в значение для дальнейшего использования
+            int a = 9, b = 10;
+            int r = Summ(a, b);
+            Console.WriteLine(r.ToString) - конвертация в текстовый формат;
+            Console.WriteLine(r); */
+
+            /* 
+            List<int> list = [1, 123, 123, 45, 6, 8, 34, -213, -123, -9]; - динамическая коллекция
+            int[] nums = { 123, 123, 123, 123, 123, 12 }; - массив
+            // int result = ListSumm(list);
+            int result = ListSumm2(nums);
+            Console.WriteLine(result); */
+
+            // array.Length → поле массива
+            // list.Count → свойство списка
+
+            /* Работа с файлами и строками
+            string word = "nice";
+            word += "!!";
+
+            // char[] sym = { 'n', 'i', 'g', 'g', 'a' };
+            // Console.WriteLine(word.Length);
+            // string a = String.Concat(word, "!");
+            // Console.WriteLine(a);
+            List<char> letters = word.ToList();
+            letters.Reverse();
+            string naword = new string(letters.ToArray());
+            Console.WriteLine(String.Compare(Convert.ToString(naword), "nice!!")); */
+            /* Если имеется полное сходство -> 0
+               Имеется сравнимое слово, но либо не тот регистр либо лишний символ (реверс приводит к -1) -> -1
+               Нет сходства в целом (возможно имеются схожие символы не в том порядке) -> 1*/
+
+
+            /* //  разбиение по символу 
+            string STnames = "Biba,Boda, Buba";
+            List<string> names = STnames
+                .Split(',')
+                .Select(x => x.Trim()) // удаление Всех пробелов
+                .ToList();
+            string a = String.Join(" ", names);
+            // char[] chars = STnames.ToCharArray(); - работает только со строками
+            // foreach (string name in names)
+            //     Console.WriteLine(name);
+
+            ToArray()   List → array
+            ToCharArray()   string → char[]
+            Split() string → string[] по разделителю
+            // Также все методы со строками создают новую строку и поэтому требуют записи результата в новую переменную
+            Console.WriteLine(a.Substring(0, a.Length - 1)); // обрезает основной кусок от последнего элемента */
+
+            /* Создание, запись и чтение файла через 
+            библиотеку System.IO c созданием изолированной среды для работы*/
+
+            /* Создание файла с собственным текстом, введенным от пользователя   
+             Console.Write("Введите текст для помещения в новый файл: ");
+            string text = Console.ReadLine();
+            using (FileStream stream = new FileStream("File.txt", FileMode.OpenOrCreate))
+            {
+                byte[] arr = System.Text.Encoding.Default.GetBytes(text);
+
+                stream.Write(arr, 0, arr.Length);
+            } */
+            /* 
+            Чтение файла
+            using (FileStream stream1 = File.OpenRead("File.txt"))
+            {
+                byte[] arr1 = new byte[stream1.Length];
+                stream1.Read(arr1, 0, arr1.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(arr1);
+                Console.WriteLine(textFromFile);
+            } */
         }
+        /* Создание функций (методов класса)
+          Невозвратимая функция
+        
+        public static void calc(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+           Возвратимая функция
+        // public static int ListSumm(List<int> x)
+        // {
+        //     int basis = 0;
+        //     foreach (int a in x)
+        //     {
+        //         basis += a;
+        //     }
+        //     return basis;
+        // }
+        public static int ListSumm2(int[] x)
+        {
+            int basis = 0;
+            foreach (int a in x)
+            {
+                basis += a;
+            }
+            return basis;
+        } */
     }
 }
